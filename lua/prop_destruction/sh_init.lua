88e33270dedc69a2a8e45e8e5efb3c67f033c12d
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-01-18 01:27:09 +0700

    Delete sh_init.lua

--- lua/prop_destruction/sh_init.lua
+++
@@ -1,9 +0,0 @@
---[[---------------------------------------------------------------------------
-	Meta functions
----------------------------------------------------------------------------]]
-
-local meta = FindMetaTable("Entity")
-
-function meta:massToStrength(mass)
-    return mass * 2.25
-end
