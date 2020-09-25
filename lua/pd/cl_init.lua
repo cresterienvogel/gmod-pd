6053e22c52371917017dab0c0db32c6a2f246ed8
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-09-25 16:12:30 +0700

    Update cl_init.lua

--- lua/pd/cl_init.lua
+++ lua/pd/cl_init.lua
@@ -5,35 +5,35 @@ surface.CreateFont("pd_hud", {size = 26, weight = 300, antialias = true, extende
 surface.CreateFont("pd_hud_shadow", {size = 27, weight = 300, antialias = true, extended = true, blursize = 1.5, font = "Roboto Condensed"})
 
 --[[
-    HUD
+	HUD
 ]]
 
 local function PrettyText(text, font, x, y, color, xalign, yalign)
-    draw.SimpleText(text, font .. "_shadow", x + 1, y + 1, ColorAlpha(color_black, 120), xalign, yalign and yalign or TEXT_ALIGN_TOP)
-    draw.SimpleText(text, font, x + 1, y + 1, ColorAlpha(color_black, 150), xalign, yalign and yalign or TEXT_ALIGN_TOP)
-    draw.SimpleText(text, font, x, y, color, xalign, yalign and yalign or TEXT_ALIGN_TOP)
+	draw.SimpleText(text, font .. "_shadow", x + 1, y + 1, ColorAlpha(color_black, 120), xalign, yalign and yalign or TEXT_ALIGN_TOP)
+	draw.SimpleText(text, font, x + 1, y + 1, ColorAlpha(color_black, 150), xalign, yalign and yalign or TEXT_ALIGN_TOP)
+	draw.SimpleText(text, font, x, y, color, xalign, yalign and yalign or TEXT_ALIGN_TOP)
 end
 
 hook.Add("HUDPaint", "Prop Destruction", function()
-    if not GetConVar("pd_hud"):GetBool() then
-        return
-    end
+	if not GetConVar("pd_hud"):GetBool() then
+		return
+	end
 
-    local tr = LocalPlayer():GetEyeTraceNoCursor()
-    if IsValid(tr.Entity) and tr.HitPos:DistToSqr(LocalPlayer():EyePos()) < 22500 then
-        local ent = tr.Entity
+	local tr = LocalPlayer():GetEyeTraceNoCursor()
+	if IsValid(tr.Entity) and tr.HitPos:DistToSqr(LocalPlayer():EyePos()) < 22500 then
+		local ent = tr.Entity
 
-        if not ent:IsDestructible() or ent:GetMaxHealth() <= 1 then 
-            return 
-        end
+		if not ent:IsDestructible() or ent:GetMaxHealth() <= 1 then 
+			return 
+		end
 
-        PrettyText("Health: " .. math.Round(ent:Health()) .. "/" .. math.Round(ent:GetMaxHealth()), "pd_hud", ScrW() / 2, ScrH() / 1.85 + 20, color_white, TEXT_ALIGN_CENTER)
-        PrettyText(ent:GetStable() and "Stable" or "Unstable", "pd_hud", ScrW() / 2, ScrH() / 1.85 + 45, HSVToColor(ent:GetStable() and 140 or -20, 1, 1), TEXT_ALIGN_CENTER)
+		PrettyText("Health: " .. math.Round(ent:Health()) .. "/" .. math.Round(ent:GetMaxHealth()), "pd_hud", ScrW() / 2, ScrH() / 1.85 + 20, color_white, TEXT_ALIGN_CENTER)
+		PrettyText(ent:GetStable() and "Stable" or "Unstable", "pd_hud", ScrW() / 2, ScrH() / 1.85 + 45, HSVToColor(ent:GetStable() and 140 or -20, 1, 1), TEXT_ALIGN_CENTER)
 	end
 end)
 
 --[[
-    Cracks
+	Cracks
 ]]
 
 local cracks = Material("crester/props/cracks")
@@ -44,9 +44,9 @@ end
 
 local prop_queue = {}
 hook.Add("OnEntityCreated", "Prop Destruction", function(ent)
-    if not GetConVar("pd_cracks"):GetBool() or not ent:IsDestructible() or ent:GetMaxHealth() <= 1 then
-        return
-    end
+	if not GetConVar("pd_cracks"):GetBool() or not ent:IsDestructible() or ent:GetMaxHealth() <= 1 then
+		return
+	end
     
 	table.insert(prop_queue, ent)
 end)
@@ -54,8 +54,8 @@ end)
 hook.Add("Tick", "Prop Destruction", function()
 	for i, v in ipairs(prop_queue) do
 		if not IsValid(v) then
-            table.remove(prop_queue, i)
-        elseif v.RenderOverride == nil then
+			table.remove(prop_queue, i)
+		elseif v.RenderOverride == nil then
 			v.RenderOverride = RenderOverride
 			table.remove(prop_queue, i)
 		end
@@ -65,17 +65,15 @@ end)
 function PD.DrawCrackedModel(ent)
 	ent:DrawModel()
 
-    if not GetConVar("pd_cracks"):GetBool() or ent:Health() > ent:GetMaxHealth() / 2 then 
-        return 
-    end
+	if not GetConVar("pd_cracks"):GetBool() or ent:Health() > ent:GetMaxHealth() / 2 then 
+		return 
+	end
 
-    cracks:SetTexture("$detail", Material(#ent:GetMaterial() > 0 and ent:GetMaterial() or (ent:GetMaterials()[1])):GetTexture("$basetexture"))
+	cracks:SetTexture("$detail", Material(#ent:GetMaterial() > 0 and ent:GetMaterial() or (ent:GetMaterials()[1])):GetTexture("$basetexture"))
     
-    render.MaterialOverride(cracks)
-    render.SetBlend(0.9)
-
-    ent:DrawModel()
-
-    render.SetBlend(1)
-    render.MaterialOverride()
+	render.MaterialOverride(cracks)
+	render.SetBlend(0.9)
+	ent:DrawModel()
+	render.SetBlend(1)
+	render.MaterialOverride()
 end
